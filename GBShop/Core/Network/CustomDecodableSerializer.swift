a648f42baa56115949f4638eabc0ba8962f59f71
therealniks <therealniks@gmail.com>
2021-08-01 14:31:36 +0300

    add Abstract error parser porotocol

---
+++ GBShop/Core/Network/CustomDecodableSerializer.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  GBShop
+//
+//  Created by therealniks on 01.08.2021.
+//
+
+import Foundation
---
+++ GBShop/Core/Network/ErrorParser.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorParser.swift
+//  GBShop
+//
+//  Created by therealniks on 01.08.2021.
+//
+
+import Foundation
