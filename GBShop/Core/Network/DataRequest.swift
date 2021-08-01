4cdd118421e5fcf7c9065a942297b7eda2b54fe6
therealniks <therealniks@gmail.com>
2021-08-01 14:47:31 +0300

    custom decodable serializer

---
+++ GBShop/Core/Network/DataRequest.swift
@@ -0,0 +1,33 @@
+//
+//  DataRequest.swift
+//  GBShop
+//
+//  Created by therealniks on 01.08.2021.
+//
+
+import Foundation
+import Alamofire
+
+protocol AbstractRequestFactory {
+    var errorParser: AbstractErrorParser { get }
+    var sessionManager: Session { get }
+    var queue: DispatchQueue { get }
+    
+    @discardableResult
+    func request<T: Decodable>(
+        request: URLRequestConvertible,
+        completion: @escaping (AFDataResponse<T>) -> Void) -> DataRequest
+}
+
+
+extension AbstractRequestFactory {
+    @discardableResult
+    public func request<T: Decodable>(
+        request: URLRequestConvertible,
+        completion: @escaping (AFDataResponse<T>) -> Void) -> DataRequest {
+        return sessionManager
+            .request(request)
+            .responseCodable(errorParser: errorParser, queue: queue, completion: completion)
+    }
+}
+
