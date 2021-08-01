7dfa6b40bdd24628505220c1a5920977cd569e97
therealniks <therealniks@gmail.com>
2021-08-01 14:31:51 +0300

    add error parser

--- GBShop/Core/Network/ErrorParser.swift
+++ GBShop/Core/Network/ErrorParser.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class ErrorParser: AbstractErrorParser {
+    func parse(_ result: Error) -> Error {
+        return result
+    }
+    
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+        return error
+    }
+}
