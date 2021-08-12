42b26bb517aeb19ed272f5cb0580de89b66227ca
therealniks <therealniks@gmail.com>
2021-08-12 15:40:06 +0300

    add Product result

---
+++ GBShop/App/Entity/Goods/ProductResult.swift
@@ -0,0 +1,22 @@
+//
+//  ProductResult.swift
+//  GBShop
+//
+//  Created by therealniks on 12.08.2021.
+//
+
+import Foundation
+
+struct ProductResult: Codable {
+    let result: Int
+    let name: String
+    let price: Int
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case result
+        case name = "product_name"
+        case price = "product_price"
+        case description = "product_description"
+    }
+}
